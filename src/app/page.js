6e380d93d196372dc526e39df7b793c9f30f7b6b
aalan-aavalos <avalosalan789@gmail.com>
2024-10-05 17:31:08 -0600

    video

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,6 @@
 import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
 import styles from "./page.module.css";
 
-import "nes.css/css/nes.min.css";
-
 export default function Home() {
   return (
     <div>
@@ -21,6 +19,38 @@ export default function Home() {
           <Button color="inherit">Menú</Button>
         </Toolbar>
       </AppBar>
+      <section></section>
+
+      <div className={styles.container}>
+        <div className={styles.videoContainer}>
+          <video autoPlay loop muted playsInline className={styles.video}>
+            <source
+              src="https://svs.gsfc.nasa.gov/vis/a000000/a003400/a003442/TychoTour_HD720.mp4"
+              type="video/mp4"
+            />
+            Your browser does not support the video tag.
+          </video>
+          <div className={styles.content}>
+            <h1>Bienvenido a mi página</h1>
+            <p>Texto sobre el video de fondo</p>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <h1>Bienvenido a mi página</h1>
+        <p>Texto sobre el video de fondo</p>
+      </div>
+
+      <div>
+        <h1>Bienvenido a mi página</h1>
+        <p>Texto sobre el video de fondo</p>
+      </div>
+
+      <div>
+        <h1>Bienvenido a mi página</h1>
+        <p>Texto sobre el video de fondo</p>
+      </div>
     </div>
   );
 }
