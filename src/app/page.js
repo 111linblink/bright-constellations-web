37c13a83e89113d0f86936433dc0479f2adc7979
111linblink <146273461+111linblink@users.noreply.github.com>
2024-10-05 21:02:11 -0600

    cards

--- src/app/page.js
+++ src/app/page.js
@@ -43,97 +43,138 @@ export default function Home() {
       </div>
 
       <div>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
       </div>
 
       {/* Cards Section */}
-      <section style={{ display: 'flex', justifyContent: 'space-around', marginTop: '20px' }}>
-        {/* Tarjeta 1 */}
-        <Card sx={{ maxWidth: 345 }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="140"
-              image="/static/images/cards/contemplative-reptile.jpg"
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Terrestrial
-              </Typography>
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              A type of exoplanet made of rock or metal, smaller than Earth, possibly with oceans or atmospheres (artist's concept).
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              Share
-            </Button>
-          </CardActions>
-        </Card>
+      <section style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
+        {/* Row 1 - Three Cards */}
+        <div style={{ display: 'flex', justifyContent: 'space-around', width: '100%' }}>
+          {/* Card 1 */}
+          <Card sx={{ maxWidth: 345, borderRadius: '5%' }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                src="https://science.nasa.gov/wp-content/uploads/2023/11/terrestrial.jpg?w=1280&format=webp&crop=1"
+                alt="Terrestrial"
+              />
+              <CardContent sx={{ textAlign: 'center' }}>
+                <Typography gutterBottom variant="h5" component="div">
+                  Terrestrial
+                </Typography>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  A type of exoplanet made of rock or metal, smaller than Earth, possibly with oceans or atmospheres (artist's concept).
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+            </CardActions>
+          </Card>
 
-        {/* Tarjeta 2 */}
-        <Card sx={{ maxWidth: 345 }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="140"
-              image="/static/images/cards/contemplative-reptile.jpg"
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Super Earth
-              </Typography>
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              Super-Earth exoplanets are also rocky, but between Earth and Neptune in size (artist's concept).
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              Learn More
-            </Button>
-          </CardActions>
-        </Card>
+          {/* Card 2 */}
+          <Card sx={{ maxWidth: 345, borderRadius: '5%' }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image="https://science.nasa.gov/wp-content/uploads/2023/11/neptunelike.jpg?w=1280&format=webp&crop=1"
+                alt="Super Earth"
+              />
+              <CardContent sx={{ textAlign: 'center' }}>
+                <Typography gutterBottom variant="h5" component="div">
+                  Super Earth
+                </Typography>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  Super-Earth exoplanets are also rocky, but between Earth and Neptune in size (artist's concept).
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+            </CardActions>
+          </Card>
 
-        {/* Tarjeta 3 */}
-        <Card sx={{ maxWidth: 345 }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="140"
-              src="/static/images/cards/contemplative-reptile.jpg"
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Neptune-like
-              </Typography>
-              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              This variety of exoplanet is similar in size to Uranus and Neptune, with an atmosphere of mostly hydrogen or helium (artist's concept).
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              Discover
-            </Button>
-          </CardActions>
-        </Card>
+          {/* Card 3 */}
+          <Card sx={{ maxWidth: 345, borderRadius: '5%' }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                src="https://science.nasa.gov/wp-content/uploads/2023/11/gasgiant.jpg?w=1280&format=webp&crop=1"
+                alt="Neptune-like"
+              />
+              <CardContent sx={{ textAlign: 'center' }}>
+                <Typography gutterBottom variant="h5" component="div">
+                  Neptune-like
+                </Typography>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  This variety of exoplanet is similar in size to Uranus and Neptune, with an atmosphere of mostly hydrogen or helium (artist's concept).
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+            </CardActions>
+          </Card>
+        </div>
+
+        {/* Row 2 - Two Cards */}
+        <div style={{ display: 'flex', justifyContent: 'space-around', width: '70%', marginTop: '30px', marginBottom: '20px' }}>          {/* Card 4 */}
+          <Card sx={{ maxWidth: 345, borderRadius: '5%' }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                src="https://science.nasa.gov/wp-content/uploads/2023/11/gasgiant.jpg?w=1280&format=webp&crop=1"
+                alt="Gas Giant"
+              />
+              <CardContent sx={{ textAlign: 'center' }}>
+                <Typography gutterBottom variant="h5" component="div">
+                  Gas Giant
+                </Typography>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  This variety of exoplanet is similar in size to Uranus and Neptune, with an atmosphere of mostly hydrogen or helium (artist's concept).
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+            </CardActions>
+          </Card>
+
+          {/* Card 5 */}
+          <Card sx={{ maxWidth: 345, borderRadius: '5%' }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                src="https://science.nasa.gov/wp-content/uploads/2023/11/unknown.jpg?w=1280&format=webp&crop=1"
+                alt="Unknown"
+              />
+              <CardContent sx={{ textAlign: 'center' }}>
+                <Typography gutterBottom variant="h5" component="div">
+                  Unknown
+                </Typography>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  This variety of exoplanet is similar in size to Uranus and Neptune, with an atmosphere of mostly hydrogen or helium (artist's concept).
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+            </CardActions>
+          </Card>
+        </div>
       </section>
-      <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
       {/* Additional Content */}
       <div>
